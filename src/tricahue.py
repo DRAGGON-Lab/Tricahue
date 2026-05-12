48b563f6107bd3be699b865ebbb0080685571237
Kerem Gurkan <kegu3575@colorado.edu>
2026-05-12 15:08:54 -0600

    Adds debug and updates the request link

--- src/tricahue.py
+++ src/tricahue.py
@@ -280,9 +280,10 @@ class XDC:
             if isinstance(file, str):
                 with open(file, 'rb') as fobj:
                     upload_file = {'file': (os.path.basename(file), fobj)}
-                    # print(upload_url)
+                    print(self.collection_url)
                     i = self.collection_url[:-2].rfind("/")
-                    response = requests.post(f'{self.collection_url[:i]}/{self.version}/attach', headers=headers, files=upload_file)
+                    print(i)
+                    response = requests.post(f'{self.collection_url}/attach', headers=headers, files=upload_file)
                     response.raise_for_status()
                     print(f'Uploaded attachment {upload_file["file"][0]}: {response.status_code}')
             else:
@@ -290,9 +291,10 @@ class XDC:
                 filename = getattr(file, 'filename', 'attachment')
                 fobj = getattr(file, 'stream', None) or getattr(file, 'file', None) or file
                 upload_file = {'file': (filename, fobj)}
-                # print(upload_url)
+                print(self.collection_url)
                 i = self.collection_url[:-2].rfind("/")
-                response = requests.post(f'{self.collection_url[:i]}/{self.version}/attach', headers=headers, files=upload_file)
+                print(i)
+                response = requests.post(f'{self.collection_url}/attach', headers=headers, files=upload_file)
                 response.raise_for_status()
                 print(f'Uploaded attachment {upload_file["file"][0]}: {response.status_code}')
 
